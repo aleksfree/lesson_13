a8f16d7220d896fa0632396823a1573605ba194d
Aleksey Roshchupkin <aleks.freelance.ru@yandex.ru>
2020-03-02 17:04:44 +0300

    Instal gulp-clean-css and gulp-rename.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 gulp.task('browser-sync', function() {
   browserSync.init({
       server: {
-          baseDir: "app"
+          baseDir: 'app'
       }
   });
-  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch(['app/*.html', 'app/**/*.css']).on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('browser-sync'));
+/* Минифицирует css файлы */
+gulp.task('styles', function() {
+  return gulp.src('app/css/**/*.css')
+  .pipe(cleanCss({compatibility: 'ie8'}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'browser-sync'));
